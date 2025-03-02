fa6c32c9293b54dac98f11807526935575101cc0
pvlbrzn <pavelberezan1998@gmail.com>
2025-03-02 11:34:53 +0300

    add filters, search_fields

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,9 +8,15 @@ from .serializers import BookSerializer
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Только авт. пользователи могут добавлять книги
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author', 'description', 'year']
+    search_fields = ['title', 'author']  # Поиск по названию и автору
+    ordering_fields = ['author', 'title']  # Сортировка
 
 
 # Получение, обновление и удаление одной книги (GET, PUT, DELETE)
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
