8d0cf8b307d3b422c925c3a82b9e88e8902afe82
pvlbrzn <pavelberezan1998@gmail.com>
2025-03-02 10:18:39 +0300

    Initial commit

--- books/views.py
+++ books/views.py
@@ -1,8 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookViewSet(viewsets.ModelViewSet):
+# Методы GET + POST
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+# Получение, обновление и удаление одной книги (GET, PUT, DELETE)
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
